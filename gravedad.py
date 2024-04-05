f4b4099bd68a98582d6c674b211a65255f69b880
pianist and cats <catsandpianist@gmail.com>
2024-04-04 22:28:22 -0300

    implementación inicial y simple de un sistema de asignación de valores iniciales

--- gravedad.py
+++ gravedad.py
@@ -102,7 +102,7 @@ class Puntos:
     def actualizar_grafico(self):
         # establecer las variables que se van a usar en el grafico
         xs, ys, masas, colores = zip(*[(i.posicion.x, i.posicion.y, i.masa, i.color) for i in self.cuerpos])
-        # actualizar las pociciones
+        # actualizar las posiciones
         self.scatter.set_offsets(list(zip(xs, ys)))
         # actualizar las masas
         self.scatter.set_sizes(list(masas))
@@ -114,19 +114,78 @@ class Puntos:
     def iniciar_animacion(self):
         plt.show()
 
+# -------------------- funcion para pedir datos de planetas --------------------------
+def pedir_dato(nombre_del_dato, close = "exit", tipo_de_dato = 1): # los tipos de datos van 0 = int; 1 = float; 2 = vector; 3 = string
+    dato = input(f'{nombre_del_dato} = ')
+    
+    if tipo_de_dato == 3:
+        return dato
+    elif dato.isnumeric():
+        if tipo_de_dato == 0:
+            return int(dato)
+        elif tipo_de_dato == 1:
+            return float(dato)
+    elif tipo_de_dato == 2:
+        vector = dato.split(";")
+        if len(vector) == 2:
+            if vector[0].isnumeric() and vector[1].isnumeric():
+                return Vector2(float(vector[0]),float(vector[1]))
+        
+    elif dato == close:
+        return False
+    
+    print("dato invalido")
+    pedir_dato(nombre_del_dato, close = close, tipo_de_dato=tipo_de_dato)
+
+def pedir_datos_del_planeta():
+    nombre = pedir_dato("Nombre", tipo_de_dato=3)
+    if nombre != False:
+            
+        masa = pedir_dato("Masa", tipo_de_dato=1)
+        if masa != False:
+                
+            posicion = pedir_dato("posicion", tipo_de_dato=2)
+            if posicion != False:
+                    
+                velocidad = pedir_dato("velocidad", tipo_de_dato=2)
+                if velocidad != False:
+                        
+                    cuerpo_creado = Cuerpo("nombre", posicion, velocidad, masa)
+                    todos_los_cuerpos.append(cuerpo_creado)
+
 # Constante de gravitacion universal
 g = 0.01
 
+# ------------ crear cuerpos -------------------------
+todos_los_cuerpos = list()
+
+sigue = True
+while sigue:
+    input_del_usuario = input('>>> ')
+    input_separado = input_del_usuario.split(" ")
+    
+    if input_del_usuario == "run":
+        sigue = False
+        break
+    elif input_separado[0] == "add":
+        pedir_datos_del_planeta()
+    elif input_separado[0] == "G":
+        if len(input_separado) == 1:
+            print("""Seleccione que quiere hacer con la variable G
+            \"=\" para asignar un valor
+            \"?\" para ver el valor actual""")
+        elif input_separado[1] == "?":
+            print(g)
+        elif input_separado[1] == "=" and len(input_separado) == 3:
+            if input_separado[2].isnumeric():
+                g = float(input_separado[2])
+            else:
+                print("error de input")
+        else:
+            print("error de input")
+
 # Uso de la clase para tener la ventana
 puntos = Puntos()
-
-# ------------ crear cuerpos -------------------------
-cuerpo1 = Cuerpo("1", Vector2(0,0),Vector2(0,0),200)
-cuerpo2 = Cuerpo("2", Vector2(10,0),Vector2(0,-10),20)
-cuerpo3 = Cuerpo("3", Vector2(-5,0),Vector2(0,10),10)
-
-# asignando todos los cuerpos creados a la variable que contiene todos los cuerpos
-todos_los_cuerpos = [cuerpo1, cuerpo2, cuerpo3]
 
 # Agregar los puntos para cada cuerpo
 puntos.agregar_cuerpos(todos_los_cuerpos)
